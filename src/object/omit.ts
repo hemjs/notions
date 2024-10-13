0422cb5b085dd5e4fd1a22ffc7cd14924d52e649
augusito <kaugustus15@gmail.com>
2024-10-13 08:40:11 +0300

    fix: improve type soundness on omit

--- src/object/omit.ts
+++ src/object/omit.ts
@@ -2,17 +2,17 @@
  * A helper to do the minimal amount of work in duplicating an object but omitting some
  * props. This ends up faster than using object ...rest or reduce to filter.
  */
-export function omit<TInput extends Record<string, any>, TResult = TInput>(
-  obj: TInput,
-  exclusions: (keyof TInput)[],
-): TResult {
+export function omit<T extends Record<string, any>, K extends keyof T>(
+  obj: T,
+  exclusions: K[],
+): Omit<T, K> {
   const result: Record<string, any> = {};
 
   for (const key in obj) {
-    if (exclusions.indexOf(key) === -1 && obj.hasOwnProperty(key)) {
+    if (exclusions.indexOf(key as any) === -1 && obj.hasOwnProperty(key)) {
       result[key] = obj[key];
     }
   }
 
-  return result as TResult;
+  return result as Omit<T, K>;
 }
